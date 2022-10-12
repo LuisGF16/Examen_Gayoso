2448477aa1240fb974cc32a41462aee447f7a6d4
Dj2412 <1913110155@untels,edu.pe>
2022-10-12 09:22:44 -0500

    Pregunta02 - Listo

---
+++ Preg02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n, sumatoria;
+	
+	cout << "Ingrese el limite: "; cin >> n;
+
+	sumatoria = (((n)*(n+1))/2);
+	
+	cout << "La sumatoria es " << sumatoria << endl;
+	
+	return 0;
+	
+}
